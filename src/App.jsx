99dc930326e4ce723c3bd93442ae1375051dc9a8
Zodderino <rencisfendiyaroglu@gmail.com>
2024-03-31 16:20:50 +0300

    Adjusted the starting project template generated by create react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginPage from "./login/LoginPage";
+function App() {
+  return (
+    <div className="LoginPage">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
